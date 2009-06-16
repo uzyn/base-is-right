cd989f5a3bb4cd0945e42c9c4bd5c84ac065342e
tengkahwee <tengkahwee@f2814e4a-5a63-11de-bbaa-f1a44a2b9d47>
2009-06-16 11:52:09 +0000

    * Update database.

--- app/config/sql/app.sql
+++ app/config/sql/app.sql
@@ -1,7 +1,13 @@
-CREATE TABLE  `products` (
-`id` BIGINT NOT NULL ,
-`name` VARCHAR( 2048 ) NOT NULL ,
-`description` TEXT NOT NULL ,
-`price` FLOAT NOT NULL ,
-`created` DATETIME NOT NULL
-) ENGINE = MYISAM;
+
+CREATE TABLE IF NOT EXISTS `products` (
+  `id` bigint(20) unsigned NOT NULL auto_increment,
+  `gid` bigint(20) NOT NULL,
+  `name` varchar(2048) NOT NULL,
+  `description` text NOT NULL,
+  `price` float NOT NULL,
+  `image` varchar(2047) default NULL,
+  `query` varchar(2047) NOT NULL,
+  `created` datetime NOT NULL,
+  PRIMARY KEY  (`id`),
+  UNIQUE KEY `gid` (`gid`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
