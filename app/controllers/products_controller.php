d7c0cb90664a6574b69d7cfedf5238480d2233a9
tengkahwee <tengkahwee@f2814e4a-5a63-11de-bbaa-f1a44a2b9d47>
2009-06-16 11:45:56 +0000

    * Dummy functions are in.

--- app/controllers/products_controller.php
+++ app/controllers/products_controller.php
@@ -1,9 +1,17 @@
 <?php
 class ProductsController extends AppController {
 
+	var $uses = array('Product');
+
+	function index() {
+		$result = $this->Product->fetch_data($this->params['url']['q']);
+		pr($result);
+	}
+
 	function view() {
 		pr('s');
 	}
 	
+
 }
 ?>
