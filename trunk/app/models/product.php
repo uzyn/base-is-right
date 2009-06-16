35b8dcdb1d4c2be4861bfafad70fd31e82a58819
tengkahwee <tengkahwee@f2814e4a-5a63-11de-bbaa-f1a44a2b9d47>
2009-06-16 11:45:56 +0000

    * Dummy functions are in.

--- trunk/app/models/product.php
+++ trunk/app/models/product.php
@@ -6,5 +6,22 @@ class Product extends AppModel {
 	var $actsAs = array(
 	'Containable',
 	);
+
+	function fetch_data($query) {
+		$result = $this->getItemData($query);
+		if (empty($result)) {
+		#do something
+		} else {
+		#do something too
+		}
+		return $result;
+	}
+
+	/*
+	 * Ridz
+	 */
+	function getItemData($query) {
+		return $query;
+	}
 }
 ?>
